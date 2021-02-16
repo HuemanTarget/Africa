290f238178a2800a55ee458793854c1a929753bd
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-16 12:23:11 -0800

    create CoverImageView and link to covers json

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Joshua Basche on 2/16/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
+
