e1ab994a28e6614a2abdf558e225d0f050ee1d09
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-16 09:54:05 -0800

    first commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Joshua Basche on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
