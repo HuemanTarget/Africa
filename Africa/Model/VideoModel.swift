c702b65eaac1bcb5d7f91dbb31785a5c23f6dbf9
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-18 11:51:05 -0800

    create VideoModel

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Joshua Basche on 2/18/21.
+//
+
+import SwiftUI
+
+struct Video: Codable, Identifiable {
+  let id: String
+  let name: String
+  let headline: String
+}
