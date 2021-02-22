46209231aa8e0279daa3e83434447b8e1c12afb4
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-22 11:27:10 -0800

    create MotionAnimationView and add as background to GalleryView

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct GalleryView: View {
     var body: some View {
+      ScrollView(.vertical, showsIndicators: false) {
         Text("Gallery")
+      } //: SCROLL
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(MotionAnimationView())
     }
 }
 
