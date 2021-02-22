a0d3c842a8a09e830bf34a6241d9eb07e0b6c9d7
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-22 14:18:48 -0800

    create three different layouts for image grid in ContentView when grid button is pressed

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -14,7 +14,30 @@ struct ContentView: View {
   
   @State private var isGridViewActive: Bool = false
   
-  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn: Int = 1
+  @State private var toolbarIcon: String = "square.grid.2x2"
+  // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+  
+  //: MARK - FUCTIONS
+  
+  func gridSwitch() {
+    gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+    gridColumn = gridLayout.count
+    print("Grid Number: \(gridColumn)")
+    
+    // Toolbar Image
+    switch gridColumn {
+    case 1:
+      toolbarIcon = "square.grid.2x2"
+    case 2:
+      toolbarIcon = "square.grid.3x2"
+    case 3:
+      toolbarIcon = "rectangle.grid.1x2"
+    default:
+      toolbarIcon = "square.grid.2x2"
+    }
+  }
   
   // MARK: - BODY
   var body: some View {
@@ -33,7 +56,17 @@ struct ContentView: View {
             } //: LOOP
           } //: LIST
         } else {
-          Text("Grid view is active")
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+              ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                  AnimalGridItemView(animal: animal)
+                } //: LINK
+              } //: LOOP
+            } //: GRID
+            .padding(10)
+            .animation(.easeIn)
+          } //: SCROLL
         } //: CONDITION
       } //: GROUP
       .navigationBarTitle("Africa", displayMode: .large)
@@ -56,8 +89,9 @@ struct ContentView: View {
               print("Grid view is activated")
               isGridViewActive = true
               haptics.impactOccurred()
+              gridSwitch()
             }) {
-              Image(systemName: "square.grid.2x2")
+              Image(systemName: toolbarIcon)
                 .font(.title2)
                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
             }
